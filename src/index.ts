ebe2b902a611b7e76afebb16ed5da0279f08736b
rayyyy <nraavy@gmail.com>
2022-09-19 22:46:56 +0900

    add server

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader'
+import { loadSchemaSync } from '@graphql-tools/load'
+import { addResolversToSchema } from '@graphql-tools/schema'
+import { ApolloServer } from 'apollo-server'
+import { join } from 'path'
+
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+]
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+}
+
+const schema = loadSchemaSync(join(__dirname, '../schema.graphql'), {
+  loaders: [new GraphQLFileLoader()],
+})
+
+const schemaWithResolvers = addResolversToSchema({ schema, resolvers })
+
+const server = new ApolloServer({
+  schema: schemaWithResolvers,
+})
+
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
