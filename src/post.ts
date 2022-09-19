895184a8aea03911729b25f852aba1b827326cb6
rayyyy <nraavy@gmail.com>
2022-09-19 21:52:41 +0900

    federation

---
+++ src/post.ts
@@ -0,0 +1,45 @@
+import { buildFederatedSchema } from '@apollo/federation'
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+  type Post {
+    id: Int
+    title: String
+    content: String
+    user: User
+  }
+
+  type Query {
+    posts: [Post]
+  }
+
+  extend type User @key(fields: "id") {
+    id: Int! @external
+  }
+`
+
+const posts = [
+  { id: 1, title: 'foo', content: 'foooo', user: { id: 1 } },
+  { id: 2, title: 'bar', content: 'baaaa', user: { id: 2 } },
+  { id: 3, title: 'baz', content: 'bazzz', user: { id: 1 } },
+]
+
+const resolvers = {
+  Query: {
+    posts: () => posts,
+  },
+
+  Post: {
+    user(post: any) {
+      return { __typename: 'User', id: post.user.id }
+    },
+  },
+}
+
+const server = new ApolloServer({
+  schema: buildFederatedSchema([{ typeDefs, resolvers }]),
+})
+
+server.listen({ port: 4001 }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
---
+++ src/user.ts
@@ -0,0 +1,38 @@
+import { buildFederatedSchema } from '@apollo/federation'
+import { ApolloServer, gql } from 'apollo-server'
+
+const typeDefs = gql`
+  type User @key(fields: "id") {
+    id: Int!
+    name: String!
+  }
+
+  type Query {
+    users: [User]
+  }
+`
+
+const users = [
+  { id: 1, name: 'taro' },
+  { id: 2, name: 'jiro' },
+]
+
+const resolvers = {
+  Query: {
+    users: () => users,
+  },
+
+  User: {
+    __resolveReference(user: any) {
+      return users.find((u) => u.id === user.id)
+    },
+  },
+}
+
+const server = new ApolloServer({
+  schema: buildFederatedSchema([{ typeDefs, resolvers }]),
+})
+
+server.listen({ port: 4002 }).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`)
+})
